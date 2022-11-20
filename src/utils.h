d6138f1aa12327abc1e8d5faab33e26b3fad23a7
Gartur1337 <gakiev13@mail.ru>
2022-11-20 21:46:31 +0300

    ready-made code, but without openmp

--- src/utils.h
+++ src/utils.h
@@ -11,7 +11,6 @@
 #include <vector>
 #include <numeric>
 
-
 double genNumbers() {
     static std::mt19937 gen(time(nullptr));
     std::uniform_int_distribution<> dist(-100, 100);
@@ -19,21 +18,27 @@ double genNumbers() {
 }
 
 int calculation(std::vector <int> vec) {
-    int sum_of_elements = 0;
-    sum_of_elements = std::accumulate(vec.begin(), vec.end(), 0);
-    std::cout << sum_of_elements << std::endl;
+    auto sum_of_elements = std::accumulate(vec.begin(), vec.end(), 0);
     return sum_of_elements;
 }
 
 void parallelStarter() {
-    const unsigned vec_size = 10000000;
-    std::vector <int> vec(vec_size);
-    std::generate(vec.begin(), vec.end(), genNumbers);
-    calculation(vec);
+    const unsigned vec_size = 1000;
+    const int num_of_vectors = 8;
+    std::vector<std::vector<int> > vec;
+    std::generate_n(std::back_inserter(vec), num_of_vectors,
+    [num_of_vectors]() -> std::vector<int>
+    {
+        std::vector<int> myVec;
+        std::generate_n(std::back_inserter(myVec), vec_size, []() { return genNumbers(); });
+        return myVec;
+    });
+
+    std::for_each(vec.begin(), vec.end(), [](std::vector<int> iVec) {
+        auto sum = calculation(iVec);
+        std::cout << sum << std::endl;
+    });
+
 }
-
-
-
-
 
 #endif //LABA2_UTILS_H
