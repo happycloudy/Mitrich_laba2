c7f0c99a49d9f0c8910cf65c998f9ac3ab00b274
Gartur1337 <gakiev13@mail.ru>
2022-11-23 17:31:02 +0300

    Changes for Yuri

--- src/parallelStarter.h
+++ src/parallelStarter.h
@@ -12,21 +12,20 @@
 #include "calculation.h"
 #include "genVectors.h"
 
-void parallelStarter(int treads, const std::vector<std::vector<int>>& parentVector) {
+std::vector<int> parallelStarter(int treads, const std::vector<std::vector<int>>& parentVector) {
     const int num_of_vectors = 8;
+    std::vector<int> sumVector;
     omp_set_num_threads(treads);
 
-    unsigned int start_time =  clock();
+    #pragma omp parallel for default(none) shared(num_of_vectors, parentVector, std::cout, sumVector)
+        for (const auto& vecItem: parentVector) {
+            auto sum = calculation(vecItem);
+            sumVector.push_back(sum);
+    #pragma omp critical
+            std::cout << sum << std::endl;
+        }
 
-#pragma omp parallel for default(none) shared(num_of_vectors, parentVector, std::cout)
-    for (const auto& vecItem: parentVector) {
-        auto sum = calculation(vecItem);
-#pragma omp critical
-        std::cout << sum << std::endl;
-    }
-
-    unsigned int end_time = clock();
-    std::cout << "Runtime - " << (end_time - start_time) / 1000.0 << std::endl;
+    return sumVector;
 }
 
 void parallelStarter(int treads) {
@@ -36,15 +35,16 @@ void parallelStarter(int treads) {
     auto vec = genVector(num_of_vectors, vec_size);
     unsigned int start_time =  clock();
 
-#pragma omp parallel for default(none) shared(num_of_vectors, vec, std::cout)
-    for (const auto& vecItem: vec) {
-        auto sum = calculation(vecItem);
-#pragma omp critical
-        std::cout << sum << std::endl;
-    }
+    #pragma omp parallel for default(none) shared(num_of_vectors, vec, std::cout)
+        for (const auto& vecItem: vec) {
+            auto sum = calculation(vecItem);
+    #pragma omp critical
+            std::cout << sum << std::endl;
+        }
 
     unsigned int end_time = clock();
     std::cout << "Runtime - " << (end_time - start_time) / 1000.0 << std::endl;
+
 }
 
 #endif //LABA2_PARALLELSTARTER_H
