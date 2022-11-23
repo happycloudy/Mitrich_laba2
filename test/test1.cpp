cd74fd8e9bd17f4b6cc1959f06bf74bb9425549d
happycloudy <arion.mikola@gmail.com>
2022-11-23 05:05:54 +0300

    added test start file

---
+++ test/test1.cpp
@@ -0,0 +1,11 @@
+//
+// Created by arion on 23.11.2022.
+//
+
+
+#include <cassert>
+
+int main() {
+    assert(1 == 1);
+    return 0;
+}
---
+++ test/test2.cpp
@@ -0,0 +1,11 @@
+//
+// Created by arion on 23.11.2022.
+//
+
+#include <cassert>
+
+int main() {
+    assert(1 == 2);
+
+    return 0;
+}
